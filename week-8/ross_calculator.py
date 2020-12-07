5537a3c55fcfcf5f70a0f37748212e6a8a3ebbe5
Dan Ross <dawross@my365.bellevue.edu>
2020-12-06 21:36:04 -0600

    8.3

---
+++ week-8/ross_calculator.py
@@ -0,0 +1,19 @@
+#============================================
+# Title:  ross_calculator.py
+# Author: Dan Ross
+# Date:   6 December 2020
+# Description: Exercise 8.3
+#===========================================
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def divide(num1, num2):
+    return num1/num2
+
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
