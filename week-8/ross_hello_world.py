67a61266a840ff828e91c30b04ff5364e5ff56b2
Dan Ross <dawross@my365.bellevue.edu>
2020-12-06 21:21:03 -0600

    8.2

--- week-8/ross_hello_world.py
+++ week-8/ross_hello_world.py
@@ -1,1 +1,8 @@
+#============================================
+# Title:  ross_calculator.py
+# Author: Dan Ross
+# Date:   6 December 2020
+# Description: Exercise 8.2
+#===========================================
+
 print('Hello World')
